5028be6cc32d4782ecb123f3880a2775d4038d7b
Rich <piemonkey@gmail.com>
2014-01-14 06:15:05 +0000

    Added test to check new shuffle albums performance against old shuffle

--- test/ch/blinkenlights/android/vanilla/MediaUtilsTest.java
+++ test/ch/blinkenlights/android/vanilla/MediaUtilsTest.java
@@ -1,6 +1,9 @@
 package ch.blinkenlights.android.vanilla;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
@@ -78,4 +81,131 @@ public class MediaUtilsTest {
 		}
 	}
 
+	@Test
+	public void testPerformance() {
+		Random rand = new Random();
+		final int nAlbums = 20;
+		final int maxAlbumTracks = 20;
+		final int iterations = 1000000;
+		
+		final long start = new Date().getTime();
+		for (int runNo = 0; runNo < iterations; runNo++) {
+			List<Song> songs = new ArrayList<Song>(nAlbums * maxAlbumTracks);
+			long songId = 0;
+			for (int albumNo = 0; albumNo < nAlbums; albumNo++) {
+				int albumTracks = rand.nextInt(maxAlbumTracks - 1) + 1;
+				for (int trackNo = 0; trackNo < albumTracks; trackNo++) {
+					Song song = new Song(songId);
+					song.albumId = albumNo;
+					songs.add(song);
+					songId++;
+				}
+			}
+			oldShuffle(songs, true);
+		}
+		final long mid = new Date().getTime();
+		for (int runNo = 0; runNo < iterations; runNo++) {
+			List<Song> songs = new ArrayList<Song>(nAlbums * maxAlbumTracks);
+			long songId = 0;
+			for (int albumNo = 0; albumNo < nAlbums; albumNo++) {
+				int albumTracks = rand.nextInt(maxAlbumTracks - 1) + 1;
+				for (int trackNo = 0; trackNo < albumTracks; trackNo++) {
+					Song song = new Song(songId);
+					song.albumId = albumNo;
+					songs.add(song);
+					songId++;
+				}
+			}
+			MediaUtils.shuffle(songs, true);
+		}
+		final long end = new Date().getTime();
+		
+		System.out.println("Old shuffle time = " + (mid - start));
+		System.out.println("New shuffle time = " + (end - mid));
+				
+	}
+	
+	/**
+	 * Shuffle a Song list using Fisher-Yates algorithm.
+	 *
+	 * @param albumShuffle If true, preserve the order of tracks inside albums.
+	 */
+	private void oldShuffle(List<Song> list, boolean albumShuffle)
+	{
+		int size = list.size();
+		if (size < 2)
+			return;
+
+		Random random = MediaUtils.getRandom();
+		if (albumShuffle) {
+			Song[] songs = list.toArray(new Song[size]);
+			Song[] temp = new Song[size];
+
+			// Make sure the albums are in order
+			Arrays.sort(songs);
+
+			// This is Fisher-Yates algorithm, but it swaps albums instead of
+			// single elements.
+			for (int i = size; --i != -1; ) {
+				Song songI = songs[i];
+				if (i > 0 && songs[i - 1].albumId == songI.albumId)
+					// This index is not the start of an album. Skip it.
+					continue;
+
+				int j = random.nextInt(i + 1);
+				while (j > 0 && songs[j - 1].albumId == songs[j].albumId)
+					// This index is not the start of an album. Find the start.
+					j -= 1;
+
+				int lowerStart = Math.min(i, j);
+				int upperStart = Math.max(i, j);
+
+				if (lowerStart == upperStart)
+					// Swap with ourself. That was easy!
+					continue;
+
+				long lowerAlbum = songs[lowerStart].albumId;
+				int lowerEnd = lowerStart;
+				while (lowerEnd + 1 < size && songs[lowerEnd + 1].albumId == lowerAlbum)
+					lowerEnd += 1;
+
+				long upperAlbum = songs[upperStart].albumId;
+				int upperEnd = upperStart;
+				while (upperEnd + 1 < size && songs[upperEnd + 1].albumId == upperAlbum)
+					upperEnd += 1;
+
+				int lowerSize = lowerEnd - lowerStart + 1;
+				int upperSize = upperEnd - upperStart + 1;
+
+				if (lowerSize == 1 && upperSize == 1) {
+					// Easy, single element swap
+					Song tempSong = songs[lowerStart];
+					songs[lowerStart] = songs[upperStart];
+					songs[upperStart] = tempSong;
+				} else {
+					// Slow multi-element swap. Copy to a new array in the
+					// swapped order.
+					System.arraycopy(songs, 0, temp, 0, lowerStart); // copy elements before lower
+					System.arraycopy(songs, upperStart, temp, lowerStart, upperSize); // copy upper elements to lower spot
+					System.arraycopy(songs, lowerEnd + 1, temp, lowerStart + upperSize, upperStart - lowerEnd - 1); // copy elements between upper and lower
+					System.arraycopy(songs, lowerStart, temp, lowerStart + upperEnd - lowerEnd, lowerSize); // copy lower elements to upper spot
+					System.arraycopy(songs, upperEnd + 1, temp, upperEnd + 1, size - upperEnd - 1); // copy elements remaining elements after upper
+
+					// New array is finished. Use the old array as temp for the
+					// next iteration.
+					Song[] tempTemp = songs;
+					songs = temp;
+					temp = tempTemp;
+				}
+			}
+
+			list.clear();
+			list.addAll(Arrays.asList(songs));
+		} else {
+			Collections.shuffle(list, random);
+		}
+	}
+
+
+	
 }
